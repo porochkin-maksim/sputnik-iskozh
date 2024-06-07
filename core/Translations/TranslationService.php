7f4bd0b5c79f6afbce57d05f0dcdd3b350f571b1
m.porochkin <m.porochkin@bronevik.com>
2024-06-07 15:37:50 +0300

    Переводы

--- core/Translations/TranslationService.php
+++ core/Translations/TranslationService.php
@@ -2,49 +2,92 @@
 
 namespace Core\Translations;
 
+use App\Models\User;
 use Core\Log\LogChannel;
+use Core\Translations\Notifications\TranslationRequired;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\Log;
 
 class TranslationService
 {
-    private string $logPath = '';
-
     public function __construct()
     {
         $config = config('logging')['channels'][LogChannel::TRANSLATIONS];
-
-        $this->logPath = $config['path'];
     }
 
     public function log(string $key): void
     {
         $key = trim($key);
-        if (!exec(sprintf('grep %s %s', $key, $this->logPath))) {
-            Log::channel(LogChannel::TRANSLATIONS)->debug(sprintf('"%s": ""', $key));
+        $line = sprintf('"%s": ""', $key);
+        if ( ! exec(sprintf('grep %s %s', $line, $this->localePath()))) {
+            $content      = $this->getContent();
+            $readyContent = $this->getReadyContent();
+
+            $translationExists = false;
+            foreach ($readyContent as $item) {
+                if (str_contains($item, '"$key"')) {
+                    $content[]         = $item;
+                    $translationExists = true;
+                    break;
+                }
+            }
+
+            if ( ! $translationExists) {
+                $content[] = "    $line";
+                $user = new User([User::EMAIL => env('ADMIN_EMAIL')]);
+                $user->notify(new TranslationRequired($key));
+            }
+
+            $this->sortTranslations($content);
         }
     }
 
-    public function sortTranslations(): void
+    public function sortTranslations(array $content = []): void
     {
-        $locale     = Lang::locale();
-        $localePath = resource_path(sprintf('lang/%s.json', $locale));
-        if (!File::exists($localePath)) {
-            return;
-        }
-
-        $file    = File::get($localePath);
-        $content = array_filter(explode("\n", $file), fn($string) => !in_array($string, ['{', '}', '']));
+        $content = $content ? : $this->getContent();
         sort($content);
 
         $content = collect($content)->map(function ($string, int $key) use ($content) {
-                $ending = $key === count($content) - 1 ? '' : ',';
-                return str_replace('",', '"', $string) . $ending;
-            })->toArray()
-        ;
+            $ending = $key === count($content) - 1 ? '' : ',';
+
+            return str_replace('",', '"', $string) . $ending;
+        })->toArray();
 
         $result = array_merge(['{'], $content, ['}']);
-        File::put($localePath, implode("\n", $result));
+        File::put($this->localePath(), implode("\n", $result));
+    }
+
+    private function localePath(): string
+    {
+        return resource_path(sprintf('lang/%s.json', Lang::locale()));
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getContent(): array
+    {
+        if ( ! File::exists($this->localePath())) {
+            return [];
+        }
+
+        $file = File::get($this->localePath());
+
+        return array_filter(explode("\n", $file), fn($string) => ! in_array($string, ['{', '}', '']));
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getReadyContent(): array
+    {
+        $readyTranslations = resource_path(sprintf('lang/_%s.json', Lang::locale()));
+        if ( ! File::exists($readyTranslations)) {
+            return [];
+        }
+
+        $file = File::get($readyTranslations);
+
+        return array_filter(explode("\n", $file), fn($string) => ! in_array($string, ['{', '}', '']));
     }
 }
--- resources/lang/ru/passwords.php
+++ resources/lang/ru/passwords.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 
 return [
-    'reset'     => 'Your password has been reset.',
-    'sent'      => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token'     => 'This password reset token is invalid.',
-    'user'      => "We can't find a user with that email address.",
+    'reset'     => 'Ваш пароль обновлён',
+    'sent'      => 'Мы отправили ссылку для сброса пароля на электроную почту.',
+    'throttled' => 'Пожалуйста, подождите немного и повторите снова.',
+    'token'     => 'Ссылка для сброса пароля устарела.',
+    'user'      => "Пользователь с такой эл.почтой не найден.",
 ];
