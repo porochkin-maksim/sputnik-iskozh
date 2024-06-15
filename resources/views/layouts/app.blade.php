94992c29f4c1036171c85cccb0d9c291a19732ac
m.porochkin <m.porochkin@bronevik.com>
2024-06-15 09:38:15 +0300

    мета роботы

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,20 +2,55 @@
 
 use Core\Resources\RouteNames;
 use Core\Resources\Views\SectionNames;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+
 ?>
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible"
-          content="ie=edge">
-    <meta name="robots"
-          content="noindex">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <meta name="csrf-token"
-          content="{{ csrf_token() }}">
+    @if (Route::is(RouteNames::INDEX))
+        <meta name="robots" content="index, nofollow, noarchive">
+    @else
+        <meta name="robots" content="noindex">
+    @endif
+
+    @if (App::isProduction())
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+           (function (m, e, t, r, i, k, a) {
+               m[i]   = m[i] || function () {
+                   (m[i].a = m[i].a || []).push(arguments);
+               };
+               m[i].l = 1 * new Date();
+               for (var j = 0; j < document.scripts.length; j++) {
+                   if (document.scripts[j].src === r) {
+                       return;
+                   }
+               }
+               k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+           })
+           (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+           ym(97578808, 'init', {
+               clickmap           : true,
+               trackLinks         : true,
+               accurateTrackBounce: true,
+           });
+        </script>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/97578808" style="position:absolute; left:-9999px;" alt="" /></div>
+        </noscript>
+        <!-- /Yandex.Metrika counter -->
+        @endif
+
+    <meta name="description" content="Садоводческое некоммерческое товарищество Спутник-Искож г. Тверь">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield(SectionNames::TITLE, RouteNames::name(Route::current()->getName(), env('APP_NAME')))</title>
 
