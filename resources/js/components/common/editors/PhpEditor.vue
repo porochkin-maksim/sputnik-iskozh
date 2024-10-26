30e28316f35a34bc75b9f5af2ee491da134baec4
m.porochkin <m.porochkin@bronevik.com>
2024-10-26 11:02:22 +0300

    codemirror as html

--- resources/js/components/common/editors/PhpEditor.vue
+++ resources/js/components/common/editors/PhpEditor.vue
@@ -32,7 +32,7 @@ export default {
         return {
             content: null,
             cmOptions: {
-                mode: "text/javascript",
+                mode: "text/html",
             },
         };
     },
