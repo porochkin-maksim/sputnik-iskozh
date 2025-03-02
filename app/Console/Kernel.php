081a50c72241a5ea224a4c2f0353a34012c63b2c
m.porochkin <m.porochkin@bronevik.com>
2025-03-02 15:42:22 +0300

    планировщик

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,6 +10,8 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('schedule:clear-cache')->dailyAt('03:00');
+
         $this->scheduleQueues($schedule);
     }
 
@@ -23,15 +25,15 @@ class Kernel extends ConsoleKernel
     private function scheduleQueues(Schedule $schedule): void
     {
         $schedule->command('queue:restart')
-                 ->everyFiveMinutes()
-                 ->name('queue.restart')
-                 ->withoutOverlapping()
-        ;
+            ->everyFiveMinutes()
+            ->name('queue.restart')
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/queue/restart.log'));
 
         $schedule->command(sprintf('queue:work --queue=%s --sleep=3 --tries=3 --stop-when-empty', implode(',', QueueEnum::values())))
-                 ->everyFifteenSeconds()
-                 ->name('queue.work.normal')
-                 ->withoutOverlapping()
-        ;
+            ->everyFifteenSeconds()
+            ->name('queue.work-normal')
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/queue/work-normal.log'));
     }
 }
