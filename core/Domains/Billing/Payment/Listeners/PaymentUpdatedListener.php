042e0d15c7f0516236f1e6be5e0a1357a6bba7df
m.porochkin <m.porochkin@bronevik.com>
2025-05-19 17:47:58 +0300

    Синхронный пересчёт счёта, услуг и платежей

--- core/Domains/Billing/Payment/Listeners/PaymentUpdatedListener.php
+++ core/Domains/Billing/Payment/Listeners/PaymentUpdatedListener.php
@@ -4,29 +4,13 @@ namespace Core\Domains\Billing\Payment\Listeners;
 
 use Core\Domains\Billing\Jobs\RecalcClaimsPayedJob;
 use Core\Domains\Billing\Payment\Events\PaymentsUpdatedEvent;
-use lc;
 
 class PaymentUpdatedListener
 {
     public function handle(PaymentsUpdatedEvent $event): void
     {
-        static $invoicesIds;
-        if ( ! $invoicesIds) {
-            $invoicesIds = [];
-        }
-
         foreach ($event->getInvoiceIds() as $invoiceId) {
-            if (in_array($invoiceId, $invoicesIds, true)) {
-                continue;
-            }
-
-            $invoicesIds[] = $invoiceId;
-            if (lc::isCli()) {
-                dispatch_sync(new RecalcClaimsPayedJob($invoiceId));
-            }
-            else {
-                dispatch(new RecalcClaimsPayedJob($invoiceId));
-            }
+            dispatch_sync(new RecalcClaimsPayedJob($invoiceId));
         }
     }
 }
