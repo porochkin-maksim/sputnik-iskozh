501b2ba77978e8788fbe373100fd019e45230bde
m.porochkin <m.porochkin@bronevik.com>
2025-05-20 16:58:03 +0300

    Привязывать платежи только к незакрытым периодам

--- core/Domains/Billing/Period/Models/PeriodSearcher.php
+++ core/Domains/Billing/Period/Models/PeriodSearcher.php
@@ -2,10 +2,18 @@
 
 namespace Core\Domains\Billing\Period\Models;
 
+use App\Models\Billing\Period;
 use Core\Db\Searcher\SearcherInterface;
 use Core\Db\Searcher\SearcherTrait;
 
 class PeriodSearcher implements SearcherInterface
 {
     use SearcherTrait;
+
+    public function setIsClosed(bool $arg): static
+    {
+        $this->addWhere(Period::IS_CLOSED, SearcherInterface::EQUALS, $arg);
+
+        return $this;
+    }
 }
