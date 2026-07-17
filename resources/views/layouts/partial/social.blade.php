e9ac0d1faa289b761af806636d339776fe5c7840
m.porochkin <m.porochkin@bronevik.com>
2026-07-17 10:53:22 +0300

    ссылки

--- resources/views/layouts/partial/social.blade.php
+++ resources/views/layouts/partial/social.blade.php
@@ -8,21 +8,17 @@ $isIndexPage = $isIndexPage ?? false;
 
 <a class="social-link text-success" target="_blank" href="https://max.ru/join/B6YT7b2T85RTNJ2Nxh8g5D0qU8FYf-1NQEw6JbYVh3Q">
     <div class="logo layout-logo-image rounded-circle-image" style="background-image: url('{{ StaticFileLocator::StaticFileService()->logoSntRed()->getUrl() }}')"></div>
-    <div>Основной чат Мах</div>
+    <div>Чат Мах</div>
 </a>
 <a class="social-link text-success" target="_blank" href="https://chat.whatsapp.com/ET8X52yidq0BmKq9WrKtqb">
     <div class="logo layout-logo-image rounded-circle-image" style="background-image: url('{{ StaticFileLocator::StaticFileService()->logoSnt()->getUrl() }}')"></div>
     <div>Объявления WhatsApp</div>
 </a>
-<a class="social-link text-success" target="_blank" href="https://chat.whatsapp.com/FqC85651AFAFWuynQbF7UQ">
-    <div class="logo layout-logo-image rounded-circle-image" style="background-image: url('{{ StaticFileLocator::StaticFileService()->logoSntOrange()->getUrl() }}')"></div>
-    <div>Беседка WhatsApp</div>
-</a>
 <a class="social-link text-success" target="_blank" href="https://t.me/SputnikIskozh">
     <div class="logo layout-logo-image rounded-circle-image" style="background-image: url('{{ StaticFileLocator::StaticFileService()->logoSnt()->getUrl() }}')"></div>
-    <div>Телеграм</div>
+    <div>Объявления Телеграм</div>
 </a>
 <a class="social-link text-success" target="_blank" href="https://vk.com/sputnik.iskozh">
     <div class="logo layout-logo-image rounded-circle-image" style="background-image: url('{{ StaticFileLocator::StaticFileService()->logoSnt()->getUrl() }}')"></div>
-    <div>ВК</div>
+    <div>Объявления ВК</div>
 </a>
