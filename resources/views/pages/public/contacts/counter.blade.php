79121f673b981afc9e767b195f31e0d377f9f8e1
m.porochkin <m.porochkin@bronevik.com>
2026-08-22 08:44:32 +0300

    плашка о показаних счётчика в ЛК

--- resources/views/pages/public/contacts/counter.blade.php
+++ resources/views/pages/public/contacts/counter.blade.php
@@ -32,13 +32,27 @@ $openGraph->setUrl(route(RouteNames::REQUESTS_COUNTER));
     <div class="row">
         <div class="col-lg-6 col-md-7 col-12">
             <div class="page-section page-card p-3 p-lg-4">
-                <div class="alert alert-info">
-                    <div>Здесь вы можете сообщить текущие показания электричества без посещения Правления.</div>
-                    <div>Отметку в членской книжке можно будет проставить потом.</div>
-                </div>
-                <counter-form :prop-account='@json(new AccountResource(lc::account()))'
-                              :prop-user='@json(new UserResource(lc::user()))'
-                              :prop-counters='@json(new ResourseList(lc::counters(), CounterResource::class))'></counter-form>
+                @php
+                    $hasProfileCounters = lc::counters()->isNotEmpty();
+                @endphp
+
+                @if ($hasProfileCounters)
+                    <div class="alert alert-success mb-0">
+                        <div>У вас уже есть привязанные счётчики в личном кабинете.</div>
+                        <div>Передавайте показания в разделе
+                            <a href="{{ route(RouteNames::PROFILE_COUNTERS) }}">«Счётчики»</a>
+                            вашего личного кабинета.
+                        </div>
+                    </div>
+                @else
+                    <div class="alert alert-info mb-0">
+                        <div>Здесь вы можете сообщить текущие показания электричества без посещения Правления.</div>
+                        <div>Отметку в членской книжке можно будет проставить потом.</div>
+                    </div>
+                    <counter-form :prop-account='@json(new AccountResource(lc::account()))'
+                                  :prop-user='@json(new UserResource(lc::user()))'
+                                  :prop-counters='@json(new ResourseList(lc::counters(), CounterResource::class))'></counter-form>
+                @endif
             </div>
         </div>
     </div>
